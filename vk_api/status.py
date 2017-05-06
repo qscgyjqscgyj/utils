c07ed9821cc00f502d9af0dd2387666959129718
aliestarten <aliestarten@gmail.com>
2017-05-06 19:11:48 +0700

    asd

--- vk_api/status.py
+++ vk_api/status.py
@@ -1,3 +1,4 @@
+import requests
 import schedule
 import time
 import vk
@@ -11,13 +12,18 @@ TRAIN_FINISH_TIME = datetime.datetime(2017, 5, 16, 16, 30)
 
 
 def update_status():
-    session = vk.Session(access_token=VkAuthToken.objects.token(VK_ADMIN_USER_ID))
-    vk_api = vk.API(session)
-
+    access_token = VkAuthToken.objects.token(VK_ADMIN_USER_ID)
     now = datetime.datetime.now()
     new_status = str(TRAIN_FINISH_TIME - now).split('.')[0]
 
-    vk_api.status.set(text=new_status)
+    new_status_url = 'https://api.vk.com/method/status.set?' \
+                     'access_token=%(access_token)s&' \
+                     'text=%(text)s&v=5.52'\
+                     % dict(
+                            access_token=access_token,
+                            text=new_status
+                        )
+    r = requests.get(new_status_url)
 
 
 # schedule.every().hour.do(update_status)
